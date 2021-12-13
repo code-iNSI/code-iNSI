cddb60ff656e1f554f427f10483186e9c1b61c1c
code-iNSI <96064500+code-iNSI@users.noreply.github.com>
2021-12-13 15:56:47 +0100

    Update bilder_xor.py

--- Challenge2/bilder_xor.py
+++ Challenge2/bilder_xor.py
@@ -1,4 +1,4 @@
-# Bilder müssen im gleichen Ordner wie das Script sein.
+# Bilder müssen im gleichen Ordner wie das Script sein. Der QR-Code sollte zum Scannen etwas von der Kamera entfernt sein, da er sich sonst nicht scannen lässt.
 from PIL import Image, ImageChops
 
 bild1 = Image.open('image.png').convert('1')
